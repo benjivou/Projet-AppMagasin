72ef6c2b774893a42153e0fcd282219a8910e873
benjivou <benjivou2@gmail.com>
2020-01-07 18:25:24 +0100

    no message

--- Projet/app/src/test/java/com/example/myapplication/ValidationTests.java
+++ Projet/app/src/test/java/com/example/myapplication/ValidationTests.java
@@ -16,72 +16,53 @@ import org.junit.Test;
 
 public class ValidationTests {
 	
-	@Test
-	public void testPasswordValidator() {
-		PasswordValidator pv = new PasswordValidator();
-		ValidationRule vr = new ValidationRule() {
-			public boolean validate(String inPassword) {
-				return false;
-			}
-			
-			public String getMessage() {
-				return "uh oh";
-			}
-			
-		};		
-		ValidationRule rvr = new RegexValidationRule("[a-z]", "foobar?");
-		List<ValidationRule> rules = new ArrayList<ValidationRule>();
-		rules.add(vr);
-		rules.add(rvr);
-		pv.setRules(rules);
-		String testPass = "this will be inval1d n0 matter what";
-		ValidationResult validity = pv.validateWithMessages(testPass);
-		assertEquals(validity.getMessages().contains("uh oh"), true);
-		assertEquals(validity.getMessages().contains("foobar?"), true);
-	}
-	
-	@Test
-	public void testSequenceRule() {
-		PasswordValidator pv = new PasswordValidator();
-		assertEquals(pv.validate("123123"), false);
-		assertEquals(pv.validate("123abc123"), true);
-		assertEquals(pv.validate("aa"), false);		
-	}
+
 	
 	@Test
 	public void testLengthRule() {
 		PasswordValidator pv = new PasswordValidator();
-		assertEquals(pv.validate("12345678902a"), true);
+		assertEquals(pv.validate("A1a"), false);								// too short
+		assertEquals(pv.validate("A1A123abc123"), true);						// test valid
+		assertEquals(pv.validate("aA(azdae"), true);							// test valid
+		assertEquals(pv.validate("aA(azdaeazdazdafzrzgffsf'fzfzfz'f"), false); // too long
+
+	}
+	
+	@Test
+	public void testMajRule() {
+		PasswordValidator pv = new PasswordValidator();
+		assertEquals(pv.validate("12345678902a"), false);
 		assertEquals(pv.validate("123456789012345a"), false);
-		assertEquals(pv.validate("1a"), false);
-		assertEquals(pv.validate("1234a"), true);
+		assertEquals(pv.validate("1aAzfzefdfzsef"), true);
+		assertEquals(pv.validate("?azeadaA"), true);
 	}
 	
 	@Test
 	public void testAtLeastOneNumber() {
 		PasswordValidator pv = new PasswordValidator();
 		assertEquals(pv.validate("abadsfas"), false);
-		assertEquals(pv.validate("1abadsfa"), true);
-		assertEquals(pv.validate("bja1414f"), true);
+		assertEquals(pv.validate("1abadsfa"), false);
+		assertEquals(pv.validate("bja1414fA"), true);
 		assertEquals(pv.validate("#@)($*#@()$"), false);
 	}
 	
 	@Test
 	public void testAtLeastOneLowercaseLetter() {
 		PasswordValidator pv = new PasswordValidator();
-		assertEquals(pv.validate("ab14324234"), true);
+		assertEquals(pv.validate("aB14324234"), true);
 		assertEquals(pv.validate("112312312"), false);
-		assertEquals(pv.validate("badfkbja1414"), true);
+		assertEquals(pv.validate("badfkbjaB1414"), true);
 		assertEquals(pv.validate("#@)($*#@()$"), false);
 	}
 	
 	@Test
-	public void testNumberLowercaseOnly() {
+	public void testSpecialCharLowercaseUpperCaseOnly() {
 		PasswordValidator pv = new PasswordValidator();
-		assertEquals(pv.validate("ab2342"), true);
-		assertEquals(pv.validate("123ABSDLKBNSD"), false);
+		assertEquals(pv.validate("ab2342"), false);
+		assertEquals(pv.validate("123aaaaa"), false);
 		assertEquals(pv.validate("@#$#$"), false);
-		assertEquals(pv.validate("ABabc"), false);
+		assertEquals(pv.validate("1Aabc"), false);
+		assertEquals(pv.validate("Ab1i;;ekn"),true);
 	}
 	
 }
