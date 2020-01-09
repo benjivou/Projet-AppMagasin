e9ca54bad3823753d30d272b7f6e06763533cebc
benjivou <benjivou2@gmail.com>
2020-01-09 20:42:03 +0100

    Declaration of all Activities

---
+++ Projet/app/src/main/java/com/example/myapplication/controller/FirstConnexionActivity.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.controller;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+public class FirstConnexionActivity extends Activity {
+}
---
+++ Projet/app/src/main/java/com/example/myapplication/controller/ListeActivity.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.controller;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ListeActivity extends Activity {
+}
